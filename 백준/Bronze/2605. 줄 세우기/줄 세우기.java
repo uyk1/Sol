31eb792a5e9cddedc97bf46165cb161093ced69f
uyk1 <109265413+uyk1@users.noreply.github.com>
2022-08-15 21:07:01 +0900

    [Bronze II] Title: 줄 세우기, Time: 220 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		List<Integer> arr = new ArrayList<>();
+		
+		for(int i = 1; i <= N; i++) {
+			arr.add(sc.nextInt(), i); 
+		}
+		
+		for(int i = N - 1; i >= 0; i--) { 
+			System.out.print(arr.get(i) + " ");
+		}
+		
+	}
+}
