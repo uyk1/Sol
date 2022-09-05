f1e2985eb8e5ef63c4e76ba93d41a29aab093b69
uyk1 <109265413+uyk1@users.noreply.github.com>
2022-09-05 16:59:53 +0900

    [Silver V] Title: 날짜 계산, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.java
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -1,12 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int E = sc.nextInt();
-		int S = sc.nextInt();
-		int M = sc.nextInt();
+		int E = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
 		int year = 0;
 		int e = 0;
