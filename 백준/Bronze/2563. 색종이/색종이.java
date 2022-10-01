9bcce14071c9a30c7bf5904c2c8405d01771b2bd
uyk1 <109265413+uyk1@users.noreply.github.com>
2022-10-01 17:40:37 +0900

    [Bronze I] Title: 색종이, Time: 224 ms, Memory: 17896 KB -BaekjoonHub

--- 백준/Bronze/2563. 색종이/색종이.java
+++ 백준/Bronze/2563. 색종이/색종이.java
@@ -1,45 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt(); // 색종이의 수
-		
-		// 각 색종이의 가로 세로 크기는 각각 10이다.
-		
-		// 주어지는 두 수는 색종이의 왼쪽 아래 꼭짓점.
-		
-		// 색종이가 붙은 영역을 1로 채운 후 합을 구한다.
-		
-		// 총 범위는 100 * 100.
-		
-		int[][] paper = new int[100][100];
-		
-		int sum = 0; // 색종이가 붙은 영역의 합.
-		
-		for(int i = 0; i < N; i++) {
-			
-			int r = sc.nextInt();
-			
-			int c = sc.nextInt();
-			
-			for(int j = 0; j < 10; j++) {
-				for(int k = 0; k < 10; k++) {
-					
-					paper[r+j][c+k] = 1;
+		int N = sc.nextInt();
+		int[][] map = new int[101][101];
+		int cnt = 0;
+		//N번 채우기 //받으면서 좌표 받아서 동시에 처리 //이미 1인 상태이면 카운트 안 함
+		for(int k = 0; k < N; k++) {
+			int y = sc.nextInt();
+			int x = sc.nextInt();
+			for(int i = 0; i < 10; i++) {
+				for(int j = 0; j < 10; j++) {
+					if(map[x+i][y+j] == 1) continue;
+					map[x+i][y+j] = 1;
+					cnt++;
 				}
 			}
 		}
-
-		for(int i = 0; i < 100; i++) {
-			for(int j = 0; j < 100; j++) {
-				sum += paper[i][j];
-			}
-		}
-		
-		System.out.println(sum);
-	} // main
-} // class
+		System.out.println(cnt);
+	}
+}
