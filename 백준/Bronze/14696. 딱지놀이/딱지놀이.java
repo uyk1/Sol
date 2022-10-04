cad189302b6db341bdaeac4c0a643b55f3174dc1
uyk1 <109265413+uyk1@users.noreply.github.com>
2022-10-04 13:03:57 +0900

    [Bronze I] Title: 딱지놀이, Time: 1188 ms, Memory: 184176 KB -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.java
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.java
@@ -1,62 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-
-		
-		int irnd = sc.nextInt();
-
-		for (int i = 0; i < irnd; i++) {
-
-			int ac = sc.nextInt();
-			int[] A = new int[ac];
-			for (int j = 0; j < ac; j++) {
-				A[j] = sc.nextInt();
-			} // 카드 입력
-
-			int bc = sc.nextInt();
-			int[] B = new int[bc];
-			for (int j = 0; j < bc; j++) {
-				B[j] = sc.nextInt();
-			} // B 카드 입력
-			
-			game(A, B);
-
-		}
-
-	} // main
-
-	public static void game(int[] a, int[] b) {
-		for (int i = 4; i > 0; i--) {
-			int a_ = 0;
-			int b_ = 0;
-			
-			for(int j=0; j < a.length; j++) {
-				if(a[j] == i) {
-					a_++;
+		int N = sc.nextInt();
+		outer: for(int i = 0; i < N; i++) {
+			int[] arr = new int[5];
+			int[] arr2 = new int[5];
+			int a = sc.nextInt();
+			for(int j = 0; j < a; j++) {
+				int p = sc.nextInt();
+				arr[p]++;
+			}
+			int b = sc.nextInt();
+			for(int j = 0; j < b; j++) {
+				int p = sc.nextInt();
+				arr2[p]++;
+			}
+			for(int j = 4; j > 0; j--) {
+				if(arr[j] > arr2[j]) {
+					System.out.println("A");
+					continue outer;
+				}
+				else if(arr[j] < arr2[j]) {
+					System.out.println("B");
+					continue outer;
 				}
 			}
-			for(int j=0; j < b.length; j++) {
-				if(b[j] == i) {
-					b_++;
-				}
-			}
-			
-			if (a_ > b_) {
-				System.out.println("A");
-				break;
-			} else if (a_ < b_) {
-				System.out.println("B");
-				break;
-			} else {
-				if (i == 1) {
-					System.out.println("D");
-				}
-			}
+			System.out.println("D");
 		}
 	}
-
-} // class
+}
