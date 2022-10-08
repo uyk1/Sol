8e3806a5d2031f5046a5553a1676cb65b8c2de2a
uyk1 <109265413+uyk1@users.noreply.github.com>
2022-10-09 04:28:17 +0900

    [Silver IV] Title: 빙고, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/README.md
+++ 백준/Silver/2578. 빙고/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 17744 KB, 시간: 220 ms
+메모리: 17700 KB, 시간: 208 ms
 
 ### 분류
 
@@ -20,16 +20,16 @@
 
 <p>다음은 사회자가 부르는 수를 차례로 지워나간다. 예를 들어 5, 10, 7이 불렸다면 이 세 수를 지운 뒤 빙고판의 모습은 다음과 같다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="width: 179px; height: 167px;"><br>
+<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/6fc024b4-5bf7-42de-b303-406db2e3ff5b/-/preview/" style="width: 179px; height: 167px;"><br>
  </p>
 
 <p>차례로 수를 지워가다가 같은 가로줄, 세로줄 또는 대각선 위에 있는 5개의 모든 수가 지워지는 경우 그 줄에 선을 긋는다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="width: 218px; height: 218px;"></p>
+<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/b5ffac7e-7db4-4d54-bf2b-63ac7d6807d8/-/preview/" style="width: 218px; height: 218px;"></p>
 
 <p>이러한 선이 세 개 이상 그어지는 순간 "빙고"라고 외치는데, 가장 먼저 외치는 사람이 게임의 승자가 된다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="width: 237px; height: 218px;"></p>
+<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/f86e3bcc-54da-420a-8f06-9600cb06eeaa/-/preview/" style="width: 237px; height: 218px;"></p>
 
 <p>철수는 친구들과 빙고 게임을 하고 있다. 철수가 빙고판에 쓴 수들과 사회자가 부르는 수의 순서가 주어질 때, 사회자가 몇 번째 수를 부른 후 철수가 "빙고"를 외치게 되는지를 출력하는 프로그램을 작성하시오.</p>
 
--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -1,85 +1,55 @@
 import java.util.Scanner;
 
 public class Main {
-	public static int[][] bingo = new int[5][5];
-	public static int rst = 0;
-	public static int bcnt = 0;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				bingo[i][j] = sc.nextInt();
-			}
-		} // 빙고판 입력
-
-		int[] call = new int[25];
-		for (int i = 0; i < 25; i++) {
-			call[i] = sc.nextInt(); // 사회자가 부르는 숫자
-
-			for (int j = 0; j < 5; j++) {
-				for (int d = 0; d < 5; d++) {
-					if (call[i] == bingo[j][d]) {
-						bingo[j][d] = 0; // 일치하는 빙고판 위치를 0으로 전환
-						rst += 1; // 시행횟수 증가
+		int[][] map = new int[5][5];
+		int[][] call = new int[5][5];
+		for(int i = 0; i < 5; i++)
+			for(int j = 0; j < 5; j++)
+				map[i][j] = sc.nextInt();
+		for(int i = 0; i < 5; i++)
+			for(int j = 0; j < 5; j++)
+				call[i][j] = sc.nextInt();
+		int bcnt = 0; //빙고까지 콜 수
+		//빙고까지 반복
+		//행, 열 , 그리고 대각선에 속해 있다면 대각선까지 검사
+		int cnt = 0; //완성된 줄 수
+		outer: for(int d = 0; d < 5; d++) {
+			for(int z = 0; z < 5; z++) {
+				bcnt++;
+				int c = call[d][z]; //사회자 콜
+				for(int i = 0; i < 5; i++) {
+					for(int j = 0; j < 5; j++) {
+						if(map[i][j] == c) {
+							//행, 열, 대각선
+							map[i][j] = 0;
+							int cnt1 = 0;
+							int cnt2 = 0;
+							int cnt3 = 0;
+							int cnt4 = 0;
+							for(int k = 0; k < 5; k++) {
+								//행, 열
+								if(map[k][j] == 0) cnt1++;
+								if(map[i][k] == 0) cnt2++;
+								if(cnt1 == 5) cnt++;
+								if(cnt2 == 5) cnt++;
+								//대각선
+								if(i == j) {
+									if(map[k][k] == 0) cnt3++;
+								}
+								if(j == 4-i) {
+									if(map[k][4-k] == 0) cnt4++;
+								}
+								if(cnt3 == 5) cnt++;
+								if(cnt4 == 5) cnt++;
+							}
+							if(cnt >= 3) break outer;
+						}
 					}
 				}
 			}
-			rS();
-			cS();
-			lD();
-			rD(); // 탐색 메서드
-
-			if (bcnt >= 3) {
-				System.out.println(rst);
-				break; // 빙고 수가 3 이상이면 결과 출력 후 종료
-			} else {
-				bcnt = 0; // 재확인을 위해 빙고 수 초기화
-			}
-		} // 사회자가 부르는 순서 입력 후 그에 따라 빙고판의 수를 0으로 변환
-
-	}// 메인
-
-	public static void rS() {
-		for(int i = 0; i < 5; i++) {
-			int cnt = 0;
-			for(int j = 0; j < 5; j++) {
-				if(bingo[i][j] == 0) {
-					cnt += 1;
-				} // 행의 0 개수를 카운트
-			}
-			if(cnt == 5) {
-				bcnt += 1;
-			} // 행의 0 개수가 5개이면 빙고 1개 플러스
 		}
+		System.out.println(bcnt);
 	}
-
-	public static void cS() {
-		for(int i = 0; i < 5; i++) {
-			int cnt = 0;
-			for(int j = 0; j < 5; j++) {
-				if(bingo[j][i] == 0) {
-					cnt += 1;
-				} // 열의 0 개수를 카운트
-			}
-			if(cnt == 5) {
-				bcnt += 1;
-			} // 열의 0 개수가 5개이면 빙고 1개 플러스
-		}
-	}
-
-	public static void lD() {
-		if(bingo[0][0] == 0 && bingo[1][1] == 0 && bingo[2][2] == 0 && bingo[3][3] == 0 && bingo[4][4] == 0) {
-			bcnt++;
-		} // 좌에서 우 대각선 탐색
-	}
-
-	public static void rD() {
-		if(bingo[0][4] == 0 && bingo[1][3] == 0 && bingo[2][2] == 0 && bingo[3][1] == 0 && bingo[4][0] == 0) {
-			bcnt++;
-		} // 우에서 좌 대각선 탐색
-	}
-
 }
-// class
