c86b4c0cdca8d08598fe2de50b825459bc0ad051
uyk1 <109265413+uyk1@users.noreply.github.com>
2022-10-11 14:16:56 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -3,7 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 	static int N;
@@ -12,10 +13,9 @@ public class Main {
 	//연결된 집을 찾기 위한 델타 상하좌우
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
-	static int cnt; //단지 수 카운트
-	static int hcnt; //단지 내 집 카운트
 	static ArrayList<Integer> list = new ArrayList<>();
-	
+	static int hcnt;
+		
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
@@ -27,34 +27,41 @@ public class Main {
 			}
 		}
 		visited = new boolean[N][N];
-		cnt = 0; //단지 수 카운트
-		//집이 존재하는 곳이고 아직 방문하지 않은 경우 탐색 시작
+		
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < N; j++) {
-				//현재 좌표, 단지 내 집 카운트
 				if(map[i][j] != 0 && !visited[i][j]) {
-					cnt++;
-					hcnt = 0;
-					dfs(i, j); //현재 좌표
+					visited[i][j] = true;
+					hcnt = 1; //단지 내 집 카운트 초기화
+					bfs(i, j); //현재 좌표
 					list.add(hcnt);
 				}
 			}
 		}
 		Collections.sort(list); //오름차순 정렬
-		System.out.println(cnt);
+		System.out.println(list.size());
 		for(int i : list) System.out.println(i);
 	}//main
 	
-	static void dfs(int x, int y) {
-		visited[x][y] = true;
-		hcnt++;
+	static void bfs(int x, int y) {
+		//여기서 큐에 넣을 것은 좌표이므로 2차원 LinkedList 활용
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {x, y}); //탐색 시작 좌표
 		
-		for(int i = 0; i < 4; i++) {
-			int dx = x + dr[i];
-			int dy = y + dc[i];
+		while(!q.isEmpty()) {
+			int cx = q.peek()[0];
+			int cy = q.peek()[1];
+			q.poll();
 			
-			if(dx >= 0 && dx < N && dy >= 0 && dy < N && map[dx][dy] != 0 && !visited[dx][dy]) {
-				dfs(dx, dy);
+			for(int i = 0; i < 4; i++) {
+				int dx = cx + dr[i];
+				int dy = cy + dc[i];
+				//한 레벨을 다 탐색 후 다음 단계로 이동
+				if(dx >= 0 && dx < N && dy >= 0 && dy < N && map[dx][dy] != 0 && !visited[dx][dy]) {
+					visited[dx][dy] = true;
+					q.add(new int[] {dx, dy});
+					hcnt++;
+				}
 			}
 		}
 	}
