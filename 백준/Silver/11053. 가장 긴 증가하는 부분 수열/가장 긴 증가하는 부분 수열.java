cf8e8cb1a17682868009d512afbb8595b754227a
uyk1 <109265413+uyk1@users.noreply.github.com>
2022-11-10 15:38:23 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 160 ms, Memory: 16056 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,22 +9,23 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
-		arr = new int[N+1];
-		for(int i = 1; i <= N; i++) arr[i] = sc.nextInt();
-		dp = new int[1001];//각 자리까지의 최장 증가 부분수열의 길이를 저장할 배열
-		//수열 A의 수를 하나씩 탐색
-		for(int i = 1; i <= N; i++) {
-			dp[i] = 1; // 부분수열의 최소 숫자는 1
-			for(int j = 1; j <= i; j++) { // 자기 앞쪽 탐색
-				//i번째 숫자보다 j번째 숫자가 더 작고, i번째까지의 dp값보다 j번째의 dp값 +1이 큰 경우.
-				//이는 이미 앞쪽부터 순차적으로 계산되어왔으므로 가능
-				if(arr[i] > arr[j] && dp[i] < dp[j] + 1) {
-					dp[i] = dp[j] + 1;
+		arr = new int[N];
+		dp = new int[N];
+		for(int i = 0; i < N; i++) arr[i] = sc.nextInt();
+		Arrays.fill(dp, 1001); //차후 반복문에서 계산을 위하여 최대 Ai+1 값으로 초기화
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if(arr[i] <= dp[j]) {
+					dp[j] = arr[i];
+					break;
 				}
 			}
 		}
 		int ans = 0;
-		for(int i = 0; i < 1001; i++) ans = Math.max(ans, dp[i]);
+		for(int i = 0; i < N; i++) {
+			if(dp[i] == 1001) break;
+			ans++;
+		}
 		System.out.println(ans);
 	}
 }
