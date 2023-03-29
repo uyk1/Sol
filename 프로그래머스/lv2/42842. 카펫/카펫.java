3f1a092735668516321a11e87d5aa06cbee5b241
uyk1 <109265413+uyk1@users.noreply.github.com>
2023-03-29 19:45:55 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,23 +1,12 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
         //yellow는 1일 수 있으므로 <= 까지!
         for(int i = 1; i <= yellow; i++) {
             if(yellow%i != 0) continue;
-            int tmp = yellow/i;
-            int r, h;
-            if(2 * i + 2 * (tmp + 2) == brown){
-                if(i + 2 <= tmp + 2) {
-                    answer[0] = tmp + 2;
-                    answer[1] = i + 2;
-                } else {
-                    answer[0] = i + 2;
-                    answer[1] = tmp + 2;
-                }
-                break;
+            if(2 * (i + yellow/i) + 4 == brown){
+                return new int[]{Math.max(i,yellow/i)+2,Math.min(i,yellow/i)+2};
             }
         }
-        
-        return answer;
+        return null;
     }
 }
