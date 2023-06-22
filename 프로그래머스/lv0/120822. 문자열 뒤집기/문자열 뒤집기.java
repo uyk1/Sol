3c7c095f4b4624a0019ea937a7ee9c36ed4d821b
uyk1 <109265413+uyk1@users.noreply.github.com>
2023-06-22 23:00:11 +0900

    [level 0] Title: 문자열 뒤집기, Time: 10.86 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        char[] arr = my_string.toCharArray();
+        for(int i = my_string.length()-1; i >= 0; i--) {
+            answer += arr[i];
+        }
+        return answer;
+    }
+}
