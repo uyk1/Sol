64bc7fb1e9abaf1f1362a896a059ed7ead227a8c
uyk1 <109265413+uyk1@users.noreply.github.com>
2023-06-28 21:10:27 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 3.01 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        char[] arr = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(map.containsKey(arr[i])) {
+                answer[i] = i - map.get(arr[i]);
+                map.put(arr[i], i);
+                continue;
+            }
+            map.put(arr[i], i);
+            answer[i] = -1;
+        }
+        
+        return answer;
+    }
+}
