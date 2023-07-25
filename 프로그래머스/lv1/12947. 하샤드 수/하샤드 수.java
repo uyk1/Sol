2e41493c1818b4e9ab50efedaee9b07fb724bccf
uyk1 <109265413+uyk1@users.noreply.github.com>
2023-07-25 22:37:49 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -4,7 +4,6 @@ class Solution {
         String tmp = String.valueOf(x);
         int sum = 0;
         for(int i = 0; i < tmp.length(); i++) sum += tmp.charAt(i) - '0';
-        System.out.print(sum);
         if(x % sum != 0) answer = false;
         return answer;
     }
