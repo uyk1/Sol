13c31f39b662950e30fc5675559c6513d84f70f2
uyk1 <109265413+uyk1@users.noreply.github.com>
2026-01-26 10:30:15 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 3.72 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] mats, String[][] park) {
+        int answer = -1;
+        
+        // 돗자리 정렬
+        Arrays.sort(mats);
+        
+        // 돗자리 별로 큰 것부터 확인
+        exit1:
+        for(int m = mats.length - 1; m >= 0; m--) {
+            
+            int len = mats[m]; // 확인할 매트 한 변 길이
+            int v = park.length; // 공원 세로 길이
+            int h = park[0].length; // 공원 가로 길이
+            
+            System.out.println(len);
+            
+            // 공원을 돌며 자리 확인
+            for(int i = 0; i < v; i++) {
+                // 만약 현재 위치에서 매트 길이만큼 세로 공간이 충분하지 않다면 브레이크
+                if(i + len > v) break;
+                
+                exit2:
+                for(int j = 0; j < h; j++) {
+                    // 만약 현재 위치에서 매트 길이만큼 가로 공간이 충분하지 않다면 브레이크
+                    if(j + len > h) break;
+                    // 공간이 충분하다면 공간이 비어있는지 확인
+                    for(int a = i; a < i + len; a++) {
+                        for(int b = j; b < j + len; b++) {
+                            if(!park[a][b].equals("-1")) {
+                                continue exit2;
+                            }
+                        }
+                    }
+                    // 공간 확인이 끝난 경우 종료
+                    answer = len;
+                    break exit1;
+                }
+            }    
+        }
+        
+        return answer;
+    }
+}
