70577e6c43730ce0b09f2679ef9b9ec2d857d8d6
uyk1 <109265413+uyk1@users.noreply.github.com>
2026-01-27 10:40:37 +0900

    [level 1] Title: 대충 만든 자판, Time: 3.13 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        
+        // 각 키맵을 돌면서 해당하는 키값에 접근할 수 있는 가장 적은 클릭 수를 저장(HashMap 사용, 조회시간 고려)
+        Map<Character, Integer> kMap = new HashMap<>();
+        for(String key : keymap) {
+            char[] cArr = key.toCharArray();
+            for(int i = 0; i < cArr.length; i++) {
+                char c = cArr[i];
+                // 도달하기 위한 클릭 횟수 최솟값 저장
+                kMap.put(c, Math.min(kMap.getOrDefault(c, i + 1), i + 1));
+            }
+        }
+        
+        // 각 타겟을 돌며 도달하기 위한 최소 클릭 수 합산 / 맵에 없을 경우 -1 저장
+        for(int i = 0; i < targets.length; i++) {
+            char[] cArr = targets[i].toCharArray();
+            int sum = 0;
+            for(int j = 0; j < cArr.length; j++) {
+                char c = cArr[j];
+                if(!kMap.containsKey(c)) {
+                    sum = -1;
+                    break;
+                }
+                sum += kMap.get(c);
+            }
+            answer[i] = sum;
+        }
+        
+        return answer;
+    }
+}
