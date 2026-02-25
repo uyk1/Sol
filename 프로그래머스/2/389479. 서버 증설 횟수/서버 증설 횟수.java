8485c60c7ae81b193df8e8a1a973859bf859456b
uyk1 <109265413+uyk1@users.noreply.github.com>
2026-02-25 10:18:59 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,21 +1,19 @@
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0; // 증설 횟수
-        int[] sArr = new int[24]; // 매 시간 증설되어 있는 서버의 수
+        int[] diff = new int[24]; // 차분 배열
+        int curCnt = 0; // 현재 서버 수
         
         for(int i = 0; i < 24; i++) {
-            int uCnt = players[i]; // 해당 시간 게임 이용자 수
-            // 해당 시간 필요한 서버 증설 개수
-            int tmpCnt = (uCnt / m) - sArr[i];
-            if(tmpCnt <= 0) continue;
-            // sArr 현행화
-            int tmpIdx = i;
-            while(tmpIdx < Math.min(i + k, 24)) {
-                sArr[tmpIdx] += tmpCnt;
-                tmpIdx++;
-            }
+            curCnt += diff[i]; // 만료될 서버 수 반영
+            int tmpCnt = players[i] / m - curCnt;
             
-            answer += tmpCnt;
+            if(tmpCnt > 0) {
+                answer += tmpCnt;
+                curCnt += tmpCnt;
+                
+                if(i + k < 24) diff[i + k] -= tmpCnt;
+            }
         }
         
         return answer;
