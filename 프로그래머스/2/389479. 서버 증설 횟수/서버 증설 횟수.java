e7974b2f3d6b7a3614ea74d945aaaaa05667e2b8
uyk1 <109265413+uyk1@users.noreply.github.com>
2026-02-25 09:41:44 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.28 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] players, int m, int k) {
+        int answer = 0; // 증설 횟수
+        int[] sArr = new int[24]; // 매 시간 증설되어 있는 서버의 수
+        
+        for(int i = 0; i < 24; i++) {
+            int uCnt = players[i]; // 해당 시간 게임 이용자 수
+            // 해당 시간 필요한 서버 증설 개수
+            int tmpCnt = (uCnt / m) - sArr[i];
+            if(tmpCnt <= 0) continue;
+            // sArr 현행화
+            int tmpIdx = i;
+            while(tmpIdx < 24 && tmpIdx < i + k) {
+                sArr[tmpIdx] += tmpCnt;
+                tmpIdx++;
+            }
+            
+            answer += tmpCnt;
+        }
+        
+        System.out.println(Arrays.toString(sArr));
+        
+        return answer;
+    }
+}
